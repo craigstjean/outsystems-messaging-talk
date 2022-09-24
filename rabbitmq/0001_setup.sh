d26a38ed066f966bd33597c3b2e0c329518ebebd
Craig St. Jean <craig@frozenrosesoftware.com>
2022-09-24 22:53:24 +0000

    Complete RabbitMQ samples

--- rabbitmq/0001_setup.sh
+++ rabbitmq/0001_setup.sh
@@ -6,11 +6,14 @@ docker build -t rabbitmq-demo .
 
 docker run -d --hostname rabbitmq-demo --name rabbitmq-demo \
     -p 15672:15672 \
+    -p 15673:15673 \
     -p 15674:15674 \
     -p 15675:15675 \
+    -p 15676:15676 \
     -p 15670:15670 \
     -p 15671:15671 \
     -p 1883:1883 \
+    -p 8883:8883 \
     -p 61613:61613 \
     -p 5672:5672 \
     rabbitmq-demo
---
+++ rabbitmq/cert/README.md
@@ -0,0 +1,5 @@
+# SSL Certificates
+Place your CA authority, server certificate, and server key here.
+
+If you need to generate self-signed certificates, make sure your browser trusts the certificate being used.
+
